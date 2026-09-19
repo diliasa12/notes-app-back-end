f80f305e324f5b025e1359cbfc4a24c7da676b8a
dili <muhammaddilia99@gmail.com>
2026-09-19 12:34:43 +0700

    feat:host variabel

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 import express from "express";
-import router from "./routes.js";
+import routes from "./routes.js";
 import cors from "cors";
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
+
 app.use(express.json());
-app.use(cors({ origin: "*" }));
-app.use(router);
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+app.use("/", routes);
+
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
