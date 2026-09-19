93963ea4546805d89e5d0d2a15de49f8d7d6af2b
dili <muhammaddilia99@gmail.com>
2026-09-19 12:08:19 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./routes.js";
+import cors from "cors";
+const app = express();
+const port = 3000;
+const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(router);
+app.listen(port, () => {
+  console.log(`Server running at http://${host}:${port}`);
+});
